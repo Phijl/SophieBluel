da1d9c8c33abeb0c39b2470d56a7c639143c3e3d
Julie Philippe <philippejuliepro@gmail.com>
2024-08-06 10:58:47 +0200

    Étape 2.3 : LogOut des utilisateurs

---
+++ FrontEnd/logout.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const authLink = document.getElementById("auth-link");
+  const editButton = document.getElementById("edit-button");
+
+  const token = localStorage.getItem("authToken");
+
+  if (token) {
+    // L'utilisateur est connecté
+    authLink.textContent = "logout";
+    authLink.href = "#";
+    editButton.style.display = "inline";
+
+    authLink.addEventListener("click", () => {
+      // Déconnecter l'utilisateur
+      localStorage.removeItem("authToken");
+      window.location.reload();
+    });
+  } else {
+    // L'utilisateur n'est pas connecté
+    authLink.textContent = "login";
+    authLink.href = "connexion.html";
+    editButton.style.display = "none";
+  }
+});

f87e0dd012bb32d7ab0061809b7d04d1747cc02b
Julie Philippe <philippejuliepro@gmail.com>
2024-10-01 19:53:31 +0200

    bandeau noir

--- FrontEnd/logout.js
+++ FrontEnd/logout.js
@@ -2,12 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const authLink = document.getElementById("auth-link");
   const editButton = document.getElementById("edit-button");
   const token = localStorage.getItem("authToken");
+  const bandeau = document.getElementById("bandeau");
 
   if (token) {
     // L'utilisateur est connecté
     authLink.textContent = "logout";
     authLink.href = "#";
     editButton.style.display = "inline";
+    bandeau.style.display = "flex";
 
     authLink.addEventListener("click", () => {
       // Déconnecter l'utilisateur
@@ -16,8 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   } else {
     // L'utilisateur n'est pas connecté
-    authLink.textContent = "login";
-    authLink.href = "connexion.html";
+
     editButton.style.display = "none";
+    bandeau.style.display = "none";
   }
 });
